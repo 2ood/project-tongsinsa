6799b41084be3e1b77cff687683350ed0ca0f04b
2ood <kyungmin.official0@gmail.com>
2022-12-29 19:42:39 +0900

    model : event, list model

--- backend/tongsinsa/tongsinsa_backend/models.py
+++ backend/tongsinsa/tongsinsa_backend/models.py
@@ -15,9 +15,27 @@ class CustomUser(AbstractUser):
     is_delete = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now, editable=False)
     email = models.EmailField()
+    user_image = models.URLField(default="default_user_image.png")
     is_staff = None
     is_active = None
     is_superuser = None
     last_login = None
     first_name = None
     last_name = None
+
+class Event(models.Model):
+    event_generated = models.DateTimeField(default=timezone.now, editable=False)
+    writer= models.CharField(max_length=150)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    info = models.JSONField()
+    info_image = models.URLField(default="default_info_image.png")
+    thumbnail = models.URLField(default="default_thumbnail_image.png")
+    is_delete = models.BooleanField(default=False)
+
+class list(models.Model):
+    user = models.ForeignKey('CustomUser',on_delete=models.CASCADE)
+    event = models.ForeignKey('Event',on_delete=models.CASCADE)
+    others = models.JSONField()
+    is_delete = models.BooleanField(default=False)
+    
